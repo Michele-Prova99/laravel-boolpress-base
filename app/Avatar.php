aa43298f50384cce36acf8df1d4f44443d4085cb
Michele Prova <michele.prova99@gmail.com>
2020-10-25 01:19:32 +0200

    Relazioni One to One e Seeders

---
+++ app/Avatar.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Avatar extends Model
+{
+    public $timestamps = false;
+    
+    protected $fillable = [
+        'telefono', 'avatar', 'user_id'
+    ];
+
+    public function user()
+    {
+    return $this->belongsTo('App\User');
+    }
+
+}
